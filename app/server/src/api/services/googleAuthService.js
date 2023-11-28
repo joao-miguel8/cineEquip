c5a5f6cb043f7d8024bd806028e92dfab5cd5053
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2023-11-28 12:01:04 -0500

    comment googleAuthService.js file

--- app/server/src/api/services/googleAuthService.js
+++ app/server/src/api/services/googleAuthService.js
@@ -1,16 +1,16 @@
-const DB = require("mongoose");
+// const DB = require("mongoose");
 
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
 
-const passport = require("passport");
+// const passport = require("passport");
 // const googleOauthSignIn = require("passport-google-oauth2").Strategy;
 
 // configure a express session initialization.
 
 // initialize a passport on every route call
-app.use(passport.initialize());
+// app.use(passport.initialize());
 // allow passport to use "express-session"
-app.use(passport.session());
+// app.use(passport.session());
 
-passport.use(new googleOauthSignIn());
+// passport.use(new googleOauthSignIn());
