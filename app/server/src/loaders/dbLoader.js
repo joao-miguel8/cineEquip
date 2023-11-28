8bc7b5cb417194297edca0c87f567466ee4ae6aa
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2023-11-28 12:27:03 -0500

    Change - server MongoURI to access .env folder in dbLoader.js file

--- app/server/src/loaders/dbLoader.js
+++ app/server/src/loaders/dbLoader.js
@@ -1,12 +1,12 @@
-const devConfig = require("../../config/dev");
+// const { MONGO_URI } = require("../../config/dev");
+require("dotenv").config();
 
 // MongoDB database library
 const mongoose = require("mongoose");
-console.log(process.env.DB_URI);
 
 async function DBLoader() {
 	try {
-		await mongoose.connect(devConfig.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+		await mongoose.connect(process.env.MONGO_URI);
 		console.log(`Connected to MongoDB`);
 	} catch (error) {
 		console.error(`Error Loading MongoDB ${error}`);
