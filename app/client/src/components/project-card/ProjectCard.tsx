5401a3fe7682cbaa46eef9b1f71683030f2fafa7
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2023-11-28 12:29:58 -0500

    Maintenance: no functional changes

--- app/client/src/components/project-card/ProjectCard.tsx
+++ app/client/src/components/project-card/ProjectCard.tsx
@@ -8,12 +8,13 @@ import useToggle from "../../hooks/useToggle";
 function ProjectCard({ index, title, isSelectModeActive, setIsSelectModeActive }: { index: number; title: string; isSelectModeActive: boolean; setIsSelectModeActive: (setSelectMode: boolean) => void }) {
 	const deleteModalToggle = useToggle();
 	const { isToggled, isOff, isOn, dispatch } = deleteModalToggle;
+
 	return (
 		<>
-			<div className={classNames("flex hover:bg-accent duration-300 ease-in-out rounded-md h-20", isSelectModeActive ? "border-2 border-primary" : null)}>
+			<div className={classNames("flex hover:bg-accent duration-300 ease-in-out rounded-md h-20 p-2", isSelectModeActive ? "border-2 border-primary" : null)}>
 				<div className={classNames("flex gap-2 flex-col items-center justify-center w-[6rem]")}>
 					<FaFolder color={"#4F48E2"} size={"1.8rem"} />
-					<h2 className="truncate-3">{title}</h2>
+					<h2 className="truncate-3">{title ? title : "title no"}</h2>
 				</div>
 				{isSelectModeActive && <IoIosCloseCircle size={"1.8rem"} onClick={() => dispatch("IS_ON")} className={"hover:text-red-500"} />}
 			</div>
