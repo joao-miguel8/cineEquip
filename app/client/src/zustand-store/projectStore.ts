038430e580097b480d4c56ab43158f0c05fb61f6
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2023-11-28 12:04:40 -0500

    - Add backend endpoint functions to zustand store

--- app/client/src/zustand-store/projectStore.ts
+++ app/client/src/zustand-store/projectStore.ts
@@ -1,24 +1,46 @@
 import { create } from "zustand";
 import type { ProjectType } from "../types/ProjectType";
+import { createNewProject } from "../lib/api/services/projectsServices/createNewProject";
+import { fetchProjects } from "../lib/api/services/projectsServices/fetchProjects";
+import { deleteProject } from "../lib/api/services/projectsServices/deleteProject";
 
 // Data types for global projects store
 type ProjectStoreActions = {
 	projects: ProjectType[];
-	addNewProject: (newProject: ProjectType) => void;
-	deleteSelectedProject: (chosenIndex: number) => void;
+	fetchAllProjects: () => Promise<void>;
+	addNewProject: (newProject: ProjectType) => Promise<void>;
+	deleteSelectedProject: (projectId: string) => Promise<void>;
 };
 
 // Project global variables and functions
 export const useProjectStore = create<ProjectStoreActions>(set => ({
 	projects: [],
-	addNewProject: newProject =>
+	fetchAllProjects: async () => {
+		const projects = await fetchProjects();
+		set({ projects });
+		return projects;
+	},
+	addNewProject: async newProject => {
+		const resp = await createNewProject(newProject);
+		console.log(resp);
+		const dataRes = await resp;
 		set(state => ({
-			projects: [...state.projects, newProject],
-		})),
-	deleteSelectedProject: (chosenI: number) =>
-		set(state => {
-			return {
-				projects: state.projects.filter((proj, i) => i !== chosenI),
-			};
-		}),
+			projects: [...state.projects, dataRes],
+		}));
+	},
+	deleteSelectedProject: async (projectId: string) => {
+		try {
+			// Check if projectToDelete is defined and has an _id property
+			if (projectId) {
+				await deleteProject(projectId);
+				set(state => ({
+					projects: state.projects.filter(proj => proj._id !== projectId),
+				}));
+			} else {
+				console.error("Invalid projectToDelete object:", projectId);
+			}
+		} catch (error) {
+			console.error("Error deleting project:", error);
+		}
+	},
 }));
