52f3fc4c4ed7f3c1e6644a696ad96d4b6d7ba005
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2023-11-28 23:45:15 -0500

    no changes were made for this file

--- app/client/src/components/project-card/ProjectCard.tsx
+++ app/client/src/components/project-card/ProjectCard.tsx
@@ -4,14 +4,14 @@ import classNames from "classnames";
 import { IoIosCloseCircle } from "react-icons/io";
 import DeleteProjectModal from "../delete-project-modal/DeleteProjectModal";
 import useToggle from "../../hooks/useToggle";
-import { Link } from "react-router-dom";
 
 function ProjectCard({ index, title, isSelectModeActive, setIsSelectModeActive }: { index: number; title: string; isSelectModeActive: boolean; setIsSelectModeActive: (setSelectMode: boolean) => void }) {
 	const deleteModalToggle = useToggle();
+
 	const { isToggled, isOff, isOn, dispatch } = deleteModalToggle;
 
 	return (
-		<Link to={"/projects/:projectId"}>
+		<>
 			<div className={classNames("flex hover:bg-accent duration-300 ease-in-out rounded-md h-20 p-2", isSelectModeActive ? "border-2 border-primary" : null)}>
 				<div className={classNames("flex gap-2 flex-col items-center justify-center w-[6rem]")}>
 					<FaFolder color={"#4F48E2"} size={"1.8rem"} />
@@ -20,7 +20,7 @@ function ProjectCard({ index, title, isSelectModeActive, setIsSelectModeActive }
 				{isSelectModeActive && <IoIosCloseCircle size={"1.8rem"} onClick={() => dispatch("IS_ON")} className={"hover:text-red-500"} />}
 			</div>
 			{deleteModalToggle.isOn && <DeleteProjectModal index={index} toggleDispatch={dispatch} title={title} handleIsSelectModeActive={setIsSelectModeActive} />}
-		</Link>
+		</>
 	);
 }
 
