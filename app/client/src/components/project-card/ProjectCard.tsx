905887880c482f547f9c3e29bbc6fe3332504c2e
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2023-11-29 15:44:09 -0500

    Update ProjectCard.tsx
    - Import Link from react-router
    - Change title to projectData.title
    - Add projectData as prop

--- app/client/src/components/project-card/ProjectCard.tsx
+++ app/client/src/components/project-card/ProjectCard.tsx
@@ -4,23 +4,25 @@ import classNames from "classnames";
 import { IoIosCloseCircle } from "react-icons/io";
 import DeleteProjectModal from "../delete-project-modal/DeleteProjectModal";
 import useToggle from "../../hooks/useToggle";
+import { Link } from "react-router-dom";
 
-function ProjectCard({ index, title, isSelectModeActive, setIsSelectModeActive }: { index: number; title: string; isSelectModeActive: boolean; setIsSelectModeActive: (setSelectMode: boolean) => void }) {
+function ProjectCard({ index, projectData, isSelectModeActive, setIsSelectModeActive }: { index: number; isSelectModeActive: boolean; setIsSelectModeActive: (setSelectMode: boolean) => void }) {
 	const deleteModalToggle = useToggle();
-
 	const { isToggled, isOff, isOn, dispatch } = deleteModalToggle;
 
 	return (
-		<>
-			<div className={classNames("flex hover:bg-accent duration-300 ease-in-out rounded-md h-20 p-2", isSelectModeActive ? "border-2 border-primary" : null)}>
-				<div className={classNames("flex gap-2 flex-col items-center justify-center w-[6rem]")}>
-					<FaFolder color={"#4F48E2"} size={"1.8rem"} />
-					<h2 className="truncate-3">{title ? title : "title no"}</h2>
+		<div className="relative">
+			{isSelectModeActive && <IoIosCloseCircle size={"1.8rem"} onClick={() => dispatch("IS_ON")} className={"absolute right-1 top-1 hover:text-red-500"} />}
+			<Link to={`/projects/${projectData?._id}`}>
+				<div className={classNames("flex hover:bg-accent duration-300 ease-in-out rounded-md h-20 p-2", isSelectModeActive ? "border-2 border-primary" : null)}>
+					<div className={classNames("flex gap-2 flex-col items-center justify-center w-[6rem]")}>
+						<FaFolder color={"#4F48E2"} size={"1.8rem"} />
+						<h2 className="truncate-3">{projectData?.title ? projectData.title : "title no"}</h2>
+					</div>
 				</div>
-				{isSelectModeActive && <IoIosCloseCircle size={"1.8rem"} onClick={() => dispatch("IS_ON")} className={"hover:text-red-500"} />}
-			</div>
-			{deleteModalToggle.isOn && <DeleteProjectModal index={index} toggleDispatch={dispatch} title={title} handleIsSelectModeActive={setIsSelectModeActive} />}
-		</>
+			</Link>
+			{deleteModalToggle.isOn && <DeleteProjectModal index={index} toggleDispatch={dispatch} title={projectData?.title} handleIsSelectModeActive={setIsSelectModeActive} />}
+		</div>
 	);
 }
 
