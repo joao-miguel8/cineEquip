3974dfee4e965f64ddbb6d01496ea0b9d3a29e12
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2023-11-28 23:47:20 -0500

    Change ProjectStore for delete project functionality
    - Remove deleteChosenProject function in zustand store
    - Add deleteSelectedProject from zustand store

--- app/client/src/zustand-store/projectStore.ts
+++ app/client/src/zustand-store/projectStore.ts
@@ -1,14 +1,13 @@
 import { create } from "zustand";
 import type { ProjectType } from "../types/ProjectType";
 import { createNewProject } from "../lib/api/services/projectsServices/createNewProject";
-import { deleteProject } from "../lib/api/services/projectsServices/deleteProject";
 
 // Data types for global projects store
 type ProjectStoreActions = {
 	projects: ProjectType[];
 	fetchAllProjects: (projects: ProjectType) => void;
 	addNewProject: (newProject: ProjectType) => Promise<void>;
-	deleteSelectedProject: (projectId: string) => Promise<void>;
+	deleteSelectedProject: (projectId: string) => void;
 };
 
 // Project global variables and functions
@@ -21,25 +20,19 @@ export const useProjectStore = create<ProjectStoreActions>(set => ({
 	},
 	addNewProject: async newProject => {
 		const resp = await createNewProject(newProject);
-		console.log(resp);
 		const dataRes = await resp;
 		set(state => ({
 			projects: [...state.projects, dataRes],
 		}));
 	},
-	deleteSelectedProject: async (projectId: string) => {
-		try {
-			// Check if projectToDelete is defined and has an _id property
-			if (projectId) {
-				await deleteProject(projectId);
-				set(state => ({
-					projects: state.projects.filter(proj => proj._id !== projectId),
-				}));
-			} else {
-				console.error("Invalid projectToDelete object:", projectId);
-			}
-		} catch (error) {
-			console.error("Error deleting project:", error);
+	deleteSelectedProject: (projectId: string) => {
+		// if projectId exists remove project with chosen id
+		if (projectId) {
+			set(state => ({
+				projects: state.projects.filter(proj => proj._id !== projectId),
+			}));
+		} else {
+			console.error("Invalid projectToDelete object:", projectId);
 		}
 	},
 }));
