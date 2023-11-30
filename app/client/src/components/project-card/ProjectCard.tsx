ad98d190fd2ef465749ad6726f247f2bba9d40bb
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2023-11-30 09:29:32 -0500

    - Bug add text center to title

--- app/client/src/components/project-card/ProjectCard.tsx
+++ app/client/src/components/project-card/ProjectCard.tsx
@@ -13,7 +13,7 @@ function ProjectCard({ index, projectData, isSelectModeActive, setIsSelectModeAc
 		<div className="relative">
 			{isSelectModeActive && <IoIosCloseCircle size={"1.8rem"} onClick={() => dispatch("IS_ON")} className={"absolute right-1 top-1 hover:text-red-500"} />}
 			<Link to={`/projects/${projectData?._id}`}>
-				<div className={classNames("flex hover:bg-accent duration-300 ease-in-out rounded-md h-20 p-2", isSelectModeActive ? "border-2 border-primary" : null)}>
+				<div className={classNames("text-center flex hover:bg-accent duration-300 ease-in-out rounded-md h-20 p-2", isSelectModeActive ? "border-2 border-primary" : null)}>
 					<div className={classNames("flex gap-2 flex-col items-center justify-center w-[6rem]")}>
 						<FaFolder color={"#4F48E2"} size={"1.8rem"} />
 						<h2 className="truncate-3">{projectData?.title ? projectData.title : "title no"}</h2>
