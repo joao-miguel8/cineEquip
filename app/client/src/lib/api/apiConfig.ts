3051a3dfca0ede7a84f8e0e7d9df548ca47acefa
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2023-11-30 23:41:03 -0500

    - reformat documentaion

--- app/client/src/lib/api/apiConfig.ts
+++ app/client/src/lib/api/apiConfig.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { BASE_URL } from "./BASE_URL";
 
-// This represents an Axios instance, encapsulating the config for making HTTP requests to a specific API base URL. Using, developers interact can easily interact with the API, this pre-configured instance allows default headers set for JSON content. This abstract and simplifies the process of making API requests, promoting consistency and maintainability.
+// This represents an Axios instance, encapsulating the config for making HTTP requests to a specific API base URL. Using this, developers can easily interact with the API, this pre-configured instance allows default headers set for JSON content. This abstracts and simplifies the process of making API requests, promoting consistency and maintainability.
 
 export const axiosInstance = axios.create({
 	baseURL: BASE_URL,
