6a55b1b46fe1534ad6d58829105074ef5afd0830
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2023-12-01 21:45:32 -0500

    - Add sceneName for createScene function backend request

--- app/client/src/lib/api/services/scene-services/createScene.ts
+++ app/client/src/lib/api/services/scene-services/createScene.ts
@@ -1,9 +1,9 @@
 import { axiosInstance } from "../../apiConfig";
 
-export async function createScene(projectId: string) {
+export async function createScene(projectId: string, sceneName: string) {
 	try {
-		const resp = await axiosInstance.post(`/createScene/${projectId}`);
-		return resp.data;
+		const resp = await axiosInstance.post(`/createScene/${projectId}`, { name: sceneName });
+		return await resp.data;
 	} catch (err) {
 		console.error(err);
 		throw err;
