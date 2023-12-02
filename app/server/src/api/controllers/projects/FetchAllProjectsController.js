0b4d7ebeefe4a32a54a4a440b3dbf1123829b4d4
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2023-12-01 21:44:13 -0500

    - Add mongoose populate method for FetchAllProjectsController

--- app/server/src/api/controllers/projects/FetchAllProjectsController.js
+++ app/server/src/api/controllers/projects/FetchAllProjectsController.js
@@ -2,7 +2,8 @@ const { Project } = require("../../models/projectModel");
 
 async function fetchAllProjects(req, res) {
 	try {
-		const projCollection = await Project.find();
+		// populate method: replaces the document id reference with the actual document data so when we call this function it will populate those arrays with document data
+		const projCollection = await Project.find().populate("scenes");
 		res.status(200).json(projCollection);
 	} catch (err) {
 		console.log("Error Fetching projects:", err);
