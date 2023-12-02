a95713244ddda2df9eadac295773170cef40ddad
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2023-12-01 21:43:29 -0500

    - Add createScene controller API functionality

--- app/server/src/api/controllers/scenes/CreateNewSceneController.js
+++ app/server/src/api/controllers/scenes/CreateNewSceneController.js
@@ -1,8 +1,22 @@
-const { Project } = require("../../models/projectModel");
+const { Project, Scene } = require("../../models/projectModel");
 
 async function CreateNewScene(req, res) {
 	try {
-		console.log(req.body);
+		// project ID from params
+		const projID = req.params.projectId;
+		// find project with id
+		const chosenProj = await Project.findById(projID);
+		// Create new Scene
+		const newScene = new Scene({
+			name: req.body.name,
+		});
+		// Save new Scene to DB Scene Collection
+		await newScene.save();
+		// push scene to scenes array in chosen project
+		chosenProj.scenes.push(newScene);
+		// Re-save Project to DB Project Collection
+		await chosenProj.save();
+		return newScene;
 	} catch (err) {
 		console.log(err);
 		throw err;
