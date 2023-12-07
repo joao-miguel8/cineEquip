14e60e7b960f292317c103de4886d3ee899176db
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2023-12-07 12:24:17 -0500

    - Change searchbar styling
    - Change width
    - Change border-radius

--- app/client/src/components/common/searchbar/Searchbar.tsx
+++ app/client/src/components/common/searchbar/Searchbar.tsx
@@ -2,7 +2,7 @@ import { CiSearch } from "react-icons/ci";
 
 function SearchBar({ placeholder }: { placeholder: string }) {
 	return (
-		<div aria-label="search for something" className="flex items-center gap-2 w-full sm:w-80 sm:mx-auto pl-4 p-2 text-14 border bg-[#FCFCFC] rounded-full focus-within:border-primary ">
+		<div aria-label="search for something" className="w-[24rem] flex items-center gap-2 pl-4 p-2 text-14 border bg-[#FCFCFC] rounded-md  focus-within:border-primary">
 			<CiSearch size={"1.2rem"} />
 			<input type="text" placeholder={placeholder} className="w-full outline-none" />
 		</div>
