cf0b6461ea62b7c700809bab0ed036defbc8774e
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2023-12-08 15:11:09 -0500

    Change: style icon size increase

--- app/client/src/components/common/header/Header.tsx
+++ app/client/src/components/common/header/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
 	return (
 		<header className={`mb-10 pt-4 px-2 sticky top-0 w-full h-[48px] flex items-center justify-between bg-[#F6F6F6]`}>
 			<button onClick={handleGoBackBtn} className="flex items-center gap-2">
-				<IoChevronBackOutline size={"1.5rem"} />
+				<IoChevronBackOutline size={"1.8rem"} />
 			</button>
 			<Link to={"/"}>CineEquip</Link>
 			<button className="bg-red-400 rounded-full">
