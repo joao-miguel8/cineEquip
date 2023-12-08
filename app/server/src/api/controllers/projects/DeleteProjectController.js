a679ed270da068eda590264c1def0499c73ac292
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2023-12-08 15:10:11 -0500

    Fix: deleting project was not deleting scene ref
    - Fix issue where only project was being deleted, not associated scene documents in the scenes collection

--- app/server/src/api/controllers/projects/DeleteProjectController.js
+++ app/server/src/api/controllers/projects/DeleteProjectController.js
@@ -1,15 +1,19 @@
-const { Project } = require("../../models/projectModel");
+const { Scene, Project } = require("../../models/projectModel");
 
 async function DeleteProject(req, res) {
+	const { projectId } = req.params;
+
 	try {
-		const { projectId } = req.params;
 		// if project id doesn't exist
 		if (!projectId) {
 			res.status(404).json({ message: "Project not found" });
 			throw Error; // early Return if not found
 		}
 		// delete project if id exists
-		await Project.findByIdAndDelete(projectId);
+		const project = await Project.findByIdAndDelete(projectId);
+		// delete scene from scenes collection
+		const deleteScenes = project.scenes;
+		await Scene.deleteMany({ _id: { $in: deleteScenes } });
 		// Success status/message
 		res.status(200).json({ message: "Project successfully deleted" });
 	} catch (err) {
