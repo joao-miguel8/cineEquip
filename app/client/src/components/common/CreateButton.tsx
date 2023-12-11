5ca97f90d08f2786d7d1db81a70d771d3c7a6e8d
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2023-12-11 14:40:50 -0500

    - Add common folder

--- app/client/src/components/common/CreateButton.tsx
+++ app/client/src/components/common/CreateButton.tsx
@@ -8,8 +8,8 @@ function CreateButton({ buttonName, toggleModal: { dispatch }, actionType }: { b
 				dispatch(actionType as "TOGGLE" | "IS_ON" | "IS_OFF");
 			}}
 			aria-label={buttonName}
-			className="py-2 fixed bottom-0 flex justify-center w-full">
-			<button className={"mb-2 px-5 py-2.5 min-[350px]:w-80 flex gap-2 justify-center items-center text-gray-900 hover:bg-gray-100 font-medium rounded-lg text-sm bg-primary dark:text-white  dark:hover:bg-gray-700 duration-150"}>
+			className="py-2 flex justify-center">
+			<button className={"p-2 flex gap-2 justify-center items-center text-gray-900 hover:bg-gray-100 font-medium rounded-lg text-sm bg-primary dark:text-white  dark:hover:bg-gray-700 duration-150"}>
 				<IoAdd size={"1.4rem"} />
 				<span>{buttonName}</span>
 			</button>
