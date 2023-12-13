17ade1aa03420d5c79a4307942d860317aa6888a
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2023-12-13 12:44:51 -0500

    Change: SelectedScene markup/styling Add Tab.tsx
    - Change SelectedScene.tsx markup structure and css styling for scrollable content
    - Add Tab component to SelectedScene.tsx tabs

--- app/client/src/views/selected-scene/SelectedScene.tsx
+++ app/client/src/views/selected-scene/SelectedScene.tsx
@@ -10,6 +10,7 @@ import { useQuery } from "react-query";
 import { useProjectStore } from "../../zustand-store/projectStore";
 import type { SceneType } from "../../types/SceneType";
 import type { ProjectType } from "../../types/ProjectType";
+import Tab from "../../components/common/tab";
 
 function SelectedScene() {
 	const { id } = useParams();
@@ -44,41 +45,48 @@ function SelectedScene() {
 		gear = "gear",
 	}
 	const [chosenTab, setChosenTab] = useState(Scenetabs.kits);
-
 	return (
-		//  --Main Container--
-		<div className="z-50 fixed w-full h-full top-0 left-0 flex items-center justify-center">
-			{/* stopPropagation added to stop overlay from toggling if user clicks on modal container */}
-			<div onClick={e => e.stopPropagation()} role="dialog" aria-labelledby="modal-title" className="z-50 overflow-y-auto mx-auto w-full h-full text-left bg-[#F6F6F6] rounded shadow-lg">
-				<Header route={`/projects/${projectContainingScene?._id}`} />
-				{/* --Modal Scene Title and SearchBar container-- */}
-				<div id="modal-title" className="mx-4 flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4 font-bold">
-					{/* --Modal Scene Title-- */}
-					<div className="flex gap-2 text-18 sm:text-20">
-						<span>Scene:</span>
-						<h4>{chosenScene?.name}</h4>
+		<>
+			<section className="flex flex-col h-screen">
+				{/* Top section sticky */}
+				<div className="w-full rounded shadow-lg bg-[#F6F6F6]">
+					<Header route={`/projects/${projectContainingScene?._id}`} />
+					<div className="mx-4 flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4 font-bold">
+						{/* --Modal Scene Title-- */}
+						<div className="flex gap-2 text-18 sm:text-20">
+							<span>Scene:</span>
+							<h4>{chosenScene?.name}</h4>
+						</div>
+						<SearchBar placeholder="Search" />
 					</div>
-					<SearchBar placeholder="Search" />
-				</div>
 
-				{/* more info of scene container / and btns */}
-				<div className={"mt-4 md:mx-4 md:flex md:justify-between relative"}>
-					{/* --Kits Btns and Gear Btns-- */}
-					<div className="mx-4 md:mx-0 mt-8 md:mt-0 mb-10 flex gap-6 items-end w-80">
-						{/* --Kits Btn-- */}
-						<button onClick={() => setChosenTab(Scenetabs.kits)} aria-label={`view your kit list for ${chosenScene?.name}`} className={classNames(`w-20 font-bold duration-150 border-b`, chosenTab === "kits" ? " text-primary border-primary" : "border-gray-400 text-black")}>
-							Kits
-						</button>
-						{/* --Gear Btn-- */}
-						<button onClick={() => setChosenTab(Scenetabs.gear)} aria-label={`view your gear list for ${chosenScene?.name}`} className={classNames(`w-20 font-bold duration-150 border-b`, chosenTab === "gear" ? " text-primary border-primary" : "border-gray-400 text-black")}>
-							Gear
-						</button>
+					{/* more info of scene container / and btns */}
+					<div className={"mt-4 md:mx-4 md:flex md:justify-between relative"}>
+						{/* --Kits Btns and Gear Btns-- */}
+						<div className="mx-4 md:mx-0 mt-8 md:mt-0 mb-10 flex gap-6 items-end w-80">
+							{/* --Kits Btn-- */}
+							<button onClick={() => setChosenTab(Scenetabs.kits)} aria-label={`view your kit list for ${chosenScene?.name}`} className={classNames(`w-20 font-bold duration-150 border-b`, chosenTab === "kits" ? " text-primary border-primary" : "border-gray-400 text-black")}>
+								Kits
+							</button>
+							{/* --Gear Btn-- */}
+							<button onClick={() => setChosenTab(Scenetabs.gear)} aria-label={`view your gear list for ${chosenScene?.name}`} className={classNames(`w-20 font-bold duration-150 border-b`, chosenTab === "gear" ? " text-primary border-primary" : "border-gray-400 text-black")}>
+								Gear
+							</button>
+						</div>
+						{/* drop down component */}
+						{projects && <ViewMoreSceneInfo sceneInfo={chosenScene} />}
 					</div>
-					{/* drop down component */}
-					{projects && <ViewMoreSceneInfo sceneInfo={chosenScene} />}
 				</div>
-			</div>
-		</div>
+				{/* Tab content */}
+				<Tab tabOption={chosenTab} tabName={"kits"}>
+					{/* scroll content container */}
+					<div className="w-full grow overflow-y-scroll bg-[#F6F6F6"></div>
+				</Tab>
+				<Tab tabOption={chosenTab} tabName={"gear"}>
+					<p>Gear Tab</p>
+				</Tab>
+			</section>
+		</>
 	);
 }
 
