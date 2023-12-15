36bc3d73e75099e04377eecbc0e266e39592e370
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2023-12-15 10:36:10 -0500

    - Change GearCard parent container to button

--- app/client/src/components/common/GearCard.tsx
+++ app/client/src/components/common/GearCard.tsx
@@ -1,20 +1,18 @@
 function GearCard() {
 	return (
-		<>
-			<div className="shadow-md w-fit rounded-sm overflow-clip bg-white">
-				<img src="#" alt="image of gear" className="border-gray-200 border-b h-40" />
-				<div className="px-4 py-2 flex flex-col">
-					<h3 aria-aria-label="gear name">Gear Name</h3>
-					<span aria-label="Gear Status" className="text-14 text-red-400">
-						Damaged
-					</span>
-					<div className="mt-2">
-						<h4 className="text-14 font-bold">Serial Number:</h4>
-						<span>adawdami3#2f1er4%</span>
-					</div>
+		<button className="shadow-md w-fit rounded-sm overflow-clip bg-white hover:bg-accent duration-300">
+			<img src="#" alt="image of gear" className="border-gray-200 border-b h-40" />
+			<div className="px-4 py-2 flex flex-col items-start">
+				<h3 aria-aria-label="gear name">Gear Name</h3>
+				<span aria-label="Gear Status" className="text-14 text-red-400">
+					Damaged
+				</span>
+				<div className="mt-2 flex flex-col items-start">
+					<h4 className="text-14 font-bold">Serial Number:</h4>
+					<span>adawdami3#2f1er4%</span>
 				</div>
 			</div>
-		</>
+		</button>
 	);
 }
 
