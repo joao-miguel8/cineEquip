6778e4c810ab9face093a62f4ea6c4d513b9e97e
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2023-12-14 20:05:59 -0500

    - Remove bottom margin to Header.tsx

--- app/client/src/layout/Header.tsx
+++ app/client/src/layout/Header.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Header = ({ route }: { route: string }) => {
 	return (
 		// <div className="p-4 mb-4 w-full flex justify-end items-center"></div>
-		<header className={`mt-4 mb-10 px-6 sticky top-0 w-full h-[48px] flex items-center justify-between bg-[#F6F6F6]`}>
+		<header className={`mt-4 px-6 sticky top-0 w-full h-[48px] flex items-center justify-between bg-[#F6F6F6]`}>
 			<Link to={route} className="flex items-center gap-2">
 				<IoChevronBackOutline size={"1.8rem"} />
 			</Link>
