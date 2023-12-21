ce280ef47f6a541ec9a6061e0a25321626310943
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2023-12-20 19:29:43 -0500

    - Add useQuery for gear fetching in SelectedGearPage
    - Add zustand gearStore for getting gear state

--- app/client/src/views/selected-gear-page/SelectedGearPage.tsx
+++ app/client/src/views/selected-gear-page/SelectedGearPage.tsx
@@ -1,5 +1,20 @@
+import { useQueries, useQuery } from "react-query";
+import { useGearStore } from "../../zustand-store/gearStore";
+import { fetchAllGear } from "../../api/services/gear-services/fetchAllGear";
+
 function SelectedGearPage() {
-	return <div>SelectedGearPage</div>;
+	// Zustand store
+	const getGearList = useGearStore(state => state.getAllGear);
+	const gearList = useGearStore(state => state.gear);
+
+	// Fetch gear from DB
+	const gearFetch = useQuery("gears", async () => await fetchAllGear(), {
+		onError: error => console.log(error),
+		onSuccess: gears => getGearList(gears),
+		refetchOnWindowFocus: false,
+	});
+
+	return <></>;
 }
 
 export default SelectedGearPage;
