47896cf07df3d2cfa3dd17441d50eefc5e88ccf6
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2023-12-26 13:59:42 -0500

    Add: selectedGearPage layout styling and input limits

--- app/client/src/views/selected-gear-page/SelectedGearPage.tsx
+++ app/client/src/views/selected-gear-page/SelectedGearPage.tsx
@@ -22,45 +22,80 @@ function SelectedGearPage() {
 	const { _id, name, img, status, QRCode } = selectedGear ?? {};
 
 	return (
-		<div>
-			{/* image and details */}
-			<div>
+		<div className="mb-10 mt-10 mx-4 flex justify-center">
+			<div className="w-full md:w-fit">
 				{/* image of gear */}
-				<img src={`${IMAGE_UPLOAD_URL}${img}`} alt={`image of ${name}`} className="w-full h-60" />
-				{/* name / purchase date / manufacturer / model */}
-				<form className="mt-4 mx-4 flex flex-col gap-4">
-					<input value={name} className="p-2 text-24" />
-					<label htmlFor="cost">Cost:</label>
-					<input type="text" name="cost" id="cost" placeholder="Add a gear cost?" className="p-2" />
-					<label htmlFor="purchaseDate">Purchase Date:</label>
-					<input type="date" name="purchaseDate" id="purchaseDate" placeholder="Add a purchase date" className="p-2" />
-					<label htmlFor="manufacturer">Manufacturer:</label>
-					<input type="text" name="manufacturer" id="manufacturer" placeholder="What manufacturer is this gear?" className="p-2" />
-					<label htmlFor="model">Model:</label>
-					<input type="text" name="model" id="model" placeholder="What Model is this gear?" className="p-2" />
-				</form>
-				{/* bottom gear details section */}
-				<div>
-					<form className="mx-4">
-						{/* QR code icon */}
-						<div className="mt-10 flex justify-between">
-							<span>QR code Icon</span>
-							{/* gear status drop down */}
-							<select name="gearStatus" id="gearStatus">
-								<option value="isAvailable">is Available</option>
-								<option value="inUse">in Use</option>
-								<option value="isDamaged">is Damaged</option>
-							</select>
+				<div className="flex flex-col md:flex-row gap-4 justify-center">
+					<img src={`${IMAGE_UPLOAD_URL}${img}`} alt={`image of ${name}`} className="mx-auto w-[400px] md:w-80 rounded-sm" />
+					{/* name / purchase date / manufacturer / model */}
+
+					<form className="flex flex-col gap-4 rounded-sm">
+						{/* gear name */}
+						<div className="flex gap-2 items-center">
+							<label htmlFor="name" className="flex-2 font-bold">
+								Name:
+							</label>
+							<input value={name} name="name" id="name" className="flex-1 bg-transparent outline-none" placeholder="gear name?" maxLength={20} />
 						</div>
-						{/* description */}
-						<div className="mt-10 flex flex-col">
-							<label htmlFor="description">Description:</label>
-							<textarea name="description" id="description" placeholder="Add a description" cols={30} rows={10} className="p-2"></textarea>
+						{/* gear cost */}
+						<div className="flex gap-2 items-center">
+							<label htmlFor="cost" className="flex-2 font-bold">
+								Cost:
+							</label>
+							<input type="number" name="cost" id="cost" placeholder="gear cost?" className="flex-1 bg-transparent outline-none" maxLength={12} />
+							{/* gear purchase date */}
+						</div>
+						<div className="flex gap-2 items-center">
+							<label htmlFor="purchaseDate" className="flex-2 font-bold">
+								Purchase Date:
+							</label>
+							<input type="date" name="purchaseDate" id="purchaseDate" placeholder="Add a purchase date" className="px-2 flex-1 outline-none rounded-full" />
+						</div>
+						{/* gear manufacturer */}
+						<div className="flex gap-2 items-center">
+							<label htmlFor="manufacturer" className="flex-2 font-bold">
+								Manufacturer:
+							</label>
+							<input type="text" name="manufacturer" id="manufacturer" placeholder="gear manufacturer?" className="flex-1 outline-none bg-transparent" maxLength={12} />
+						</div>
+						{/* gear model */}
+						<div className="flex gap-2 items-center">
+							<label htmlFor="model" className="flex-2 font-bold">
+								Model:
+							</label>
+							<input type="text" name="model" id="model" placeholder="gear model?" className="flex-1 outline-none bg-transparent" maxLength={12} />
 						</div>
 					</form>
 				</div>
+				{/* bottom gear details section */}
+				<div>
+					<form>
+						{/* gear QR code icon */}
+						<div className="mt-10 flex flex-col sm:flex-row sm:items-start justify-between gap-4">
+							<span>QR code Icon</span>
+							{/* gear status drop down */}
+							<div className="flex gap-2 items-center">
+								<label htmlFor="gearStatus" className="font-bold">
+									Gear Status:
+								</label>
+								<select name="gearStatus" id="gearStatus" className="p-2 outline-none rounded-full">
+									<option value="isAvailable">is Available</option>
+									<option value="inUse">in Use</option>
+									<option value="isDamaged">is Damaged</option>
+								</select>
+							</div>
+						</div>
+						{/* gear description */}
+						<div className="mt-10 flex flex-col">
+							<label htmlFor="description" className="font-bold">
+								Description:
+							</label>
+							<textarea name="description" id="description" placeholder="Add a description" cols={30} rows={8} maxLength={400} className="mt-2 p-2 resize-none outline-none rounded-sm"></textarea>
+						</div>
+					</form>
+				</div>
+				<button className="mt-4 px-4 py-2 btn-primary text-white">Update Gear</button>
 			</div>
-			<div></div>
 		</div>
 	);
 }
